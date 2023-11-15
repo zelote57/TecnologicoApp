e530b37b91b7deb4730cdc52b427db0aace59e72
Gustavo Lozada <zelote57@gmail.com>
2023-11-14 20:56:03 -0500

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TecnologicoApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
